96be7d9ecb9942505587999a04c915be12457745
Josh-Savvy <joshsavyy007@gmail.com>
2023-09-18 06:59:25 +0100

    fixed build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "tailwindcss/tailwind.css";
 import "@/public/styles/main.css";
 import { AppProps } from "next/app";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: any) => {
 	return <Component {...pageProps} />;
 };
 
